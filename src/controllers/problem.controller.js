06c519cb1babacbb971565aa30540ebaeef117ba
ltpatil <ltpatil.3088@gmail.com>
2026-02-01 14:00:47 +0530

    Added unimplemeted error with help of HTTP_STATUS_CODE library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,38 @@
+const {StatusCodes} = require('http-status-codes');
+
+
 function pingPC(req,res){
     return res.json({message : 'Problem Controller is UP'});
 }
 
 function addProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message :'Not Implemented'
+    })
 }
 
 function getProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message :'Not Implemented'
+    })
 }
 
 function getProblems(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message :'Not Implemented'
+    })
 }
 
 function deleteProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message :'Not Implemented'
+    })
 }
 
 function updateProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message :'Not Implemented'
+    })
 }
 
 module.exports ={
